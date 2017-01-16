bfd23598eed7a6372937bb2b765c92b8dcbbd39e
Tiago Henriques <trinosauro@gmail.com>
2017-01-16 18:57:16 +0100

    Use loop to show partners

--- pages/RediSchool.js
+++ pages/RediSchool.js
@@ -17,11 +17,26 @@ import {
     Blockquote,
 } from 'rebass'
 
+const partners = [
+    '/static/partners/cnn-206b5b_f9eb2f6b61e14af381fd40d97c0a6d61~mv2.png',
+    '/static/partners/handelsblatt-206b5b_baa1962353b54babb53ff0d431f8ae9c~mv2.png',
+    '/static/partners/forbes-206b5b_85be8c14daff4dbc9b53e51c929aa312~mv2.png',
+    '/static/partners/the-guardian-206b5b_a1bff4122a704edba4305e4ca73e915c~mv2.jpg',
+    '/static/partners/die-zeit-206b5b_3a6744be26374afe98174d41937dc6d6~mv2.png',
+    '/static/partners/bloomberg-206b5b_5079153fccce407985051f88d56495ef~mv2.jpg',
+    '/static/partners/washington-post-206b5b_6d848b48c8844bd597fb8345f46aa906~mv2.jpg',
+    '/static/partners/welt-n24-206b5b_7ee4cf0f2ead4fa8a7b804dbf7f202c7~mv2.png',
+    '/static/partners/ajplus-206b5b_76f5324b0a1d4481bf0dbdd5e7254c26~mv2.png',
+    '/static/partners/tedx-innovations-206b5b_009aec41271c410bb79c07c2820c2f70~mv2.jpg',
+    '/static/partners/co-exist-206b5b_981ef7b40f764642ad396d71646e0df8~mv2.png',
+    '/static/partners/deutsche-welle-206b5b_05500378376d46f0bb3342c0b0a12ad8~mv2.jpg'
+]
+
 export default () => (
     <div>
         <Helmet
             title='Welcome to ReDI School'
-        />
+            />
         <Toolbar backgroundColor='#fff' color='#000'>
             <NavItem>
                 <img src='/static/redi-school-logo-206b5b_40b9f2073c364120a38c225d5b3dabc8-mv2.jpg'
@@ -29,7 +44,7 @@ export default () => (
                         maxWidth: '239px',
                         height: 'auto',
                     }}
-                />
+                    />
             </NavItem>
             <Space auto />
             <NavItem children='Home' />
@@ -40,39 +55,39 @@ export default () => (
             <NavItem children='About us' />
             <NavItem>
                 <img src='/static/social-icons/facebook-icon-30646eb34311104aba036a018b9a226b.png'
-                />
+                    />
             </NavItem>
             <NavItem>
                 <img src='/static/social-icons/twitter-icon-d090c1c8997ee76d531500a6002f81a2.png'
-                />
+                    />
             </NavItem>
             <NavItem>
                 <img src='/static/social-icons/instagram-icon-d3645e6fa159b8ef24374656fc7b245a.png'
-                />
+                    />
             </NavItem>
         </Toolbar>
         <Banner
             backgroundImage='/static/banners/redi-school-hero-image-206b5b_cea26d3ad6714acf944c096a9cf3d254~mv2.jpg'
-        >
+            >
             <Container m={1} backgroundColor='rgba(127, 127, 127, 0.5)'>
                 <Heading
                     level={1}
                     big={true}
                     children='ReDI School'
-                />
+                    />
                 <Text
                     size={3}
                     children='Using technology to break down barriers and connect the leaders of tomorrow.'
-                />
+                    />
                 <ButtonOutline
                     children='APPLY AS A STUDENT'
                     color="white"
-                />
+                    />
                 <Space auto />
                 <ButtonOutline
                     children='JOIN AS A VOLUNTEER'
                     color="white"
-                />
+                    />
                 <Space />
             </Container>
         </Banner>
@@ -80,14 +95,14 @@ export default () => (
             <Flex
                 wrap
                 justify='space-between'
-            >
+                >
                 <Flex
                     column
                     style={{
                         width: 320,
                         minHeight: 384,
                     }}
-                >
+                    >
                     <Space auto />
                     <Heading level={3} >
                         ReDI School of Digital Integration is a non-profit digital school for tech-interested newcomers applying for asylum in Germany.
@@ -112,14 +127,14 @@ export default () => (
                     <Space auto />
                     <ButtonOutline
                         children='CONTACT US'
-                    />
+                        />
                     <Space auto />
                 </Flex>
             </Flex>
             <Space auto />
             <Banner
                 backgroundImage='/static/banners/teacher-206b5b_9f4b42eec3df4e628e86334146b5752f~mv2_d_2048_1360_s_2.jpg'
-            >
+                >
                 <Blockquote source='Sebastian Probst Eide, Teacher and CTO of a Berlin based Startup'>
                     Our students are an amazingly talented, resourceful and motivated group. Through this project we help them realize their potential, and give them a clear path into the future. I feel honoured to be allowed to contribute to their growth.
                 </Blockquote>
@@ -130,97 +145,25 @@ export default () => (
             </Heading>
             <Space auto />
             <Flex wrap justify='space-between' align='center'>
-                <Box p={1}>
-                    <img src='/static/partners/cnn-206b5b_f9eb2f6b61e14af381fd40d97c0a6d61~mv2.png'
-                        style={{
-                            width: '200px',
-                            height: 'auto'
-                        }} />
-                </Box>
-                <Box p={1}>
-                    <img src='/static/partners/handelsblatt-206b5b_baa1962353b54babb53ff0d431f8ae9c~mv2.png'
-                        style={{
-                            width: '200px',
-                            height: 'auto'
-                        }} />
-                </Box>
-                <Box p={1}>
-                    <img src='/static/partners/forbes-206b5b_85be8c14daff4dbc9b53e51c929aa312~mv2.png'
-                        style={{
-                            width: '200px',
-                            height: 'auto'
-                        }} />
-                </Box>
-                <Box p={1}>
-                    <img src='/static/partners/the-guardian-206b5b_a1bff4122a704edba4305e4ca73e915c~mv2.jpg'
-                        style={{
-                            width: '200px',
-                            height: 'auto'
-                        }} />
-                </Box>
-                <Box p={1}>
-                    <img src='/static/partners/die-zeit-206b5b_3a6744be26374afe98174d41937dc6d6~mv2.png'
-                        style={{
-                            width: '200px',
-                            height: 'auto'
-                        }} />
-                </Box>
-                <Box p={1}>
-                    <img src='/static/partners/bloomberg-206b5b_5079153fccce407985051f88d56495ef~mv2.jpg'
-                        style={{
-                            width: '200px',
-                            height: 'auto'
-                        }} />
-                </Box>
-                <Box p={1}>
-                    <img src='/static/partners/washington-post-206b5b_6d848b48c8844bd597fb8345f46aa906~mv2.jpg'
-                        style={{
-                            width: '200px',
-                            height: 'auto'
-                        }} />
-                </Box>
-                <Box p={1}>
-                    <img src='/static/partners/welt-n24-206b5b_7ee4cf0f2ead4fa8a7b804dbf7f202c7~mv2.png'
-                        style={{
-                            width: '200px',
-                            height: 'auto'
-                        }} />
-                </Box>
-                <Box p={1}>
-                    <img src='/static/partners/ajplus-206b5b_76f5324b0a1d4481bf0dbdd5e7254c26~mv2.png'
-                        style={{
-                            width: '200px',
-                            height: 'auto'
-                        }} />
-                </Box>
-                <Box p={1}>
-                    <img src='/static/partners/tedx-innovations-206b5b_009aec41271c410bb79c07c2820c2f70~mv2.jpg'
-                        style={{
-                            width: '200px',
-                            height: 'auto'
-                        }} />
-                </Box>
-                <Box p={1}>
-                    <img src='/static/partners/co-exist-206b5b_981ef7b40f764642ad396d71646e0df8~mv2.png'
-                        style={{
-                            width: '200px',
-                            height: 'auto'
-                        }} />
-                </Box>
-                <Box p={1}>
-                    <img src='/static/partners/deutsche-welle-206b5b_05500378376d46f0bb3342c0b0a12ad8~mv2.jpg'
-                        style={{
-                            width: '200px',
-                            height: 'auto'
-                        }} />
-                </Box>
+                {
+                    partners.map((partner, idx) => (
+                        <Box p={1}>
+                            <img src={partner}
+                                style={{
+                                    width: '200px',
+                                    height: 'auto'
+                                }} />
+                        </Box>
+                    ))
+                }
+
             </Flex>
         </Container>
         <Footer m={2} >
             <NavItem
                 small={true}
                 children='© 2016 By ReDI School'
-            />
+                />
             |
             <NavItem>
                 <a href='#'>IMPRINT & CONTACT</a>
@@ -228,15 +171,15 @@ export default () => (
             <Space auto />
             <NavItem>
                 <img src='/static/social-icons/facebook-icon-30646eb34311104aba036a018b9a226b.png'
-                />
+                    />
             </NavItem>
             <NavItem>
                 <img src='/static/social-icons/twitter-icon-d090c1c8997ee76d531500a6002f81a2.png'
-                />
+                    />
             </NavItem>
             <NavItem>
                 <img src='/static/social-icons/instagram-icon-d3645e6fa159b8ef24374656fc7b245a.png'
-                />
+                    />
             </NavItem>
         </Footer>
     </div >
